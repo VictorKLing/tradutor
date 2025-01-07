9f3a219bb521745645c709e3f3ee80972b75ab47
Victor Kling <victorkling.p@gmail.com>
2025-01-07 14:15:00 -0300

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const languages = [
   { code: "en", name: "Inglês" },
@@ -10,6 +10,28 @@ const languages = [
 ];
 
 function App() {
+  const [sourceLang, setSourceLang] = useState("pt");
+  const [targetLang, setTargetLang] = useState("en");
+  const [sourceText, setSourceText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [translatedText, setTranslatedText] = useState("");
+
+  useEffect(() => {
+    handleTranslate();
+  }, [sourceText]);
+
+  const handleTranslate = async () => {
+    setIsLoading(true)
+    const response = await fetch(
+      `https://api.mymemory.translated.net/get?q=${encodeURIComponent(sourceText)}&langpair=${sourceLang}|${targetLang}`
+    );    if (!response.ok) {
+      throw new Error(`HTTP ERROR: ${response.status}`);
+    }
+    const data = await response.json();
+    setTranslatedText(data.responseData.translatedText)
+    setIsLoading(false)
+  };
+
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <header className="bg-white shadow-sm">
@@ -22,7 +44,11 @@ function App() {
       <main className="flex-grow flex items-start justify-center px-4 py-8">
         <div className="w-full max-w-5xl bg-white rounded-lg shadow-md overflow-hidden">
           <div className="flex items-center justify-between p-4 border-b border-gray-200">
-            <select className="text-sm text-textColor bg-transparent border-none focus:outline-none cursor-pointer">
+            <select
+              value={sourceLang}
+              onChange={(event) => setSourceLang(event.target.value)}
+              className="text-sm text-textColor bg-transparent border-none focus:outline-none cursor-pointer"
+            >
               {languages.map((lang) => (
                 <option key={lang.code} value={lang.code}>
                   {lang.name}
@@ -45,7 +71,11 @@ function App() {
                 />
               </svg>
             </button>
-            <select className="text-sm text-textColor bg-transparent border-none focus:outline-none cursor-pointer">
+            <select
+              value={targetLang}
+              onChange={(event) => setTargetLang(event.target.value)}
+              className="text-sm text-textColor bg-transparent border-none focus:outline-none cursor-pointer"
+            >
               {languages.map((lang) => (
                 <option key={lang.code} value={lang.code}>
                   {lang.name}
@@ -57,14 +87,19 @@ function App() {
           <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="p-4">
               <textarea
+                value={sourceText}
+                onChange={(event) => setSourceText(event.target.value)}
                 placeholder="Digite seu texto..."
                 className="w-full h-40 text-lg text-textColor bg-transparent resize-none  border-none outline-none"
               ></textarea>
             </div>
             <div className="p-4 relative bg-secondaryBackground border-l border-gray-200">
               <div className="absolute inset-0 flex items-center justify-center ">
-                <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-500"></div>
-                <p className="text-lg text-textColor"></p>
+                {isLoading ? (
+                  <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-blue-500"></div>
+                ) : (
+                  <p className="text-lg text-textColor">{translatedText}</p>
+                )}
               </div>
             </div>
           </div>
