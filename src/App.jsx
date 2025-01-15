9cbbf3ebf3cdfa2ff7a4d35fbb8bff68fa286120
Victor Kling <victorkling.p@gmail.com>
2025-01-15 17:50:41 -0300

    commit 5

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
       <header className="bg-white shadow-sm">
         <div className="max-w-5xl mx-auto px-4 py-3 flex items-center">
           <h1 className="text-headerColor text-2xl font-normal">
-            Tradutor dev
+            Tradutor Dev
           </h1>
         </div>
       </header>
